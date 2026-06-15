1639a637f5cecf00b40cbfc3d1842466a1c34dcf
Aishwarya S <aishwarya161005@gmail.com>
2026-06-15 17:23:19 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,4 @@
-from calculator import add, multiply
+from calculator import add
 
 def test_add():
-    assert add(2, 3) == 5     # will fail because of the bug
-
-def test_multiply():
-    assert multiply(3, 4) == 12
+    assert add(10, 5) == 15
