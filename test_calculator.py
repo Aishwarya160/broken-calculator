df8a9a00d33140e374390aaeb7397b383d801741
Aishwarya S <aishwarya161005@gmail.com>
2026-06-15 17:25:17 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,7 @@
-from calculator import add
+from calculator import add, multiply
 
 def test_add():
-    assert add(10, 5) == 15
+    assert add(2, 3) == 5
+
+def test_multiply():
+    assert multiply(3, 4) == 12
